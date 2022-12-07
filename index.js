fd130e80baba6b09106cef8770c1b025a4d2545f
JK-coder-kun <125khantko@gmail.com>
2022-12-07 23:11:33 +0630

    make working query selectors

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-function getComputerChoice(){
+const rock=document.querySelector(".rock");
+const paper=document.querySelector(".paper");
+const scissors=document.querySelector(".scissors");
+const btn=document.querySelector(".btn");
+let playerSection;
+let computerSection;
 
+rock.addEventListener('click',function(){getPlayerChoice("rock");});
+paper.addEventListener('click',function(){getPlayerChoice("paper");});
+scissors.addEventListener('click',function(){getPlayerChoice("scissors");});
+
+
+function getComputerChoice(){
+    let randomNumber=Math.floor(Math.random()*100)%3;
+    return randomNumber==1 ? "rock" : (randomNumber==2 ? "paper" : "scissors");
 };
+
+
+function getPlayerChoice(choice){
+        playerSection= choice;
+        console.log(playerSection);
+}
+
 function playRound(playerSection,computerSection){
 
 };
+
 function game(){
     for(let i=0;i<5;i++){
         playRound();
